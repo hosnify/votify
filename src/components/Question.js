3d89c07895d9838976c716424951647bf1ebc65b
hosnify <hosnify@gmail.com>
2021-06-19 17:40:04 +0200

    improve question UI

--- src/components/Question.js
+++ src/components/Question.js
@@ -7,13 +7,47 @@ import {
   CardContent,
   Typography,
 } from "@material-ui/core";
-import { Progress } from "semantic-ui-react";
+import { Button, Icon, Progress, Transition, Modal } from "semantic-ui-react";
+
 import { useTheme } from "@material-ui/core/styles";
-import QuestionOption from "./QuestionOption";
+import { handleAddAnswer } from "../actions/answer";
+import { useState, useEffect, useRef } from "react";
 
-function Question({ authedUser, users, question, ...rest }) {
+function Question({ loggedUser, users, question, dispatch, ...rest }) {
   const theme = useTheme();
+  const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const [answer, setAnswer] = useState(users[loggedUser].answers[question.id]);
 
+  const timer = useRef();
+
+  useEffect(() => {
+    return () => {
+      clearTimeout(timer.current);
+    };
+  }, []);
+
+  const handleAnswer = (qid, answer) => {
+    if (!loading) {
+      setLoading(true);
+      timer.current = window.setTimeout(() => {
+        setSuccess(true);
+        setLoading(false);
+      }, 2000);
+    }
+    setAnswer(answer);
+  };
+
+  const confirmAnswer = () => {
+    if (!answer) {
+      alert("select one of two options first ");
+    } else {
+      dispatch(handleAddAnswer(question.id, answer));
+    }
+  };
+  const getVotes = (answer) => {
+    return question[answer].votes.length;
+  };
   return (
     <Grid container direction="row">
       <Grid item>
@@ -33,14 +67,90 @@ function Question({ authedUser, users, question, ...rest }) {
         <Card style={{ minWidth: theme.spacing(150), maxWidth: "fit-content" }}>
           <CardContent>
             <Typography color="primary" gutterBottom>
-              {question.author} asked :
+              {users[question.author].name} asked :
             </Typography>
             <Typography color="primary" gutterBottom>
               Would you rather ?
             </Typography>
-            <QuestionOption optionText={question.optionOne.text} />
-            <Progress value="4" total="5" progress="percent" />
-            <QuestionOption optionText={question.optionTwo.text} />
+            <Grid container direction="column" justify="center" spacing={5}>
+              <Grid item lg={6}>
+                <Button.Group>
+                  <Button
+                    loading={loading}
+                    disabled={!!users[loggedUser].answers[question.id]}
+                    positive={answer === "optionOne"}
+                    negative={answer === "optionTwo"}
+                    icon
+                    fluid
+                    onClick={() => handleAnswer(question.id, "optionOne")}
+                  >
+                    <Icon
+                      name={
+                        (answer === "optionOne" && "check") ||
+                        (answer === "optionTwo" && "close")
+                      }
+                    />
+                    {question.optionOne.text}
+                  </Button>
+                  <Button.Or />
+                  <Button
+                    disabled={!!users[loggedUser].answers[question.id]}
+                    loading={loading}
+                    positive={answer === "optionTwo"}
+                    negative={answer === "optionOne"}
+                    fluid
+                    onClick={() => handleAnswer(question.id, "optionTwo")}
+                  >
+                    <Icon
+                      name={
+                        (answer === "optionOne" && "close") ||
+                        (answer === "optionTwo" && "check")
+                      }
+                    />
+                    {question.optionTwo.text}
+                  </Button>
+                </Button.Group>
+              </Grid>
+              <Grid container direction="row" spacing={5} item lg={12}>
+                <Grid item lg={6}>
+                  <Transition
+                    visible={!!users[loggedUser].answers[question.id]}
+                    animation="scale"
+                    duration={500}
+                  >
+                    <Progress
+                      value={getVotes("optionOne")}
+                      total={getVotes("optionOne") + getVotes("optionTwo")}
+                      progress="ratio"
+                      indicating
+                      label="votes"
+                    />
+                  </Transition>
+                </Grid>
+                <Grid item lg={6}>
+                  <Transition
+                    visible={!!users[loggedUser].answers[question.id]}
+                    animation="scale"
+                    duration={500}
+                  >
+                    <Progress
+                      value={getVotes("optionTwo")}
+                      total={getVotes("optionOne") + getVotes("optionTwo")}
+                      progress="ratio"
+                      indicating
+                      label="votes"
+                    />
+                  </Transition>
+                </Grid>
+              </Grid>
+              {!users[loggedUser].answers[question.id] && (
+                <Grid item lg={12} container justify="flex-end">
+                  <Button basic color="green" on onClick={confirmAnswer}>
+                    Confirm Vote & hide
+                  </Button>
+                </Grid>
+              )}
+            </Grid>
           </CardContent>
         </Card>
       </Grid>
@@ -48,10 +158,10 @@ function Question({ authedUser, users, question, ...rest }) {
   );
 }
 
-function mapStateToProps({ authedUser, users, questions }, { id }) {
+function mapStateToProps({ loggedUser, users, questions }, { id }) {
   const question = questions[id];
   return {
-    authedUser,
+    loggedUser,
     users,
     question,
   };
--- src/components/questionsList.js
+++ src/components/questionsList.js
@@ -1,25 +1,14 @@
-import { connect } from "react-redux";
 import { Grid } from "@material-ui/core";
 import Question from "./Question";
 
-function QuestionsList({ ...props }) {
+export default function QuestionsList({ ...props }) {
   return (
     <Grid container direction="column" spacing={2} justify="flex-start">
-      {props.questionsIds.map((id) => (
-        <Grid item>
-          <Question id={id}></Question>
+      {props.questions.map((id) => (
+        <Grid key={id} item>
+          <Question key={id} id={id}></Question>
         </Grid>
       ))}
     </Grid>
   );
 }
-
-function mapStateToProps({ questions }) {
-  return {
-    questionsIds: Object.keys(questions).sort(
-      (a, b) => questions[b].timestamp - questions[a].timestamp
-    ),
-  };
-}
-
-export default connect(mapStateToProps)(QuestionsList);
