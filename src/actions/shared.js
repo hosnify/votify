fb55a04d320ed508115600ece78d190248c94d31
hosnify <hosnify@gmail.com>
2021-06-19 17:41:13 +0200

    remove mock user

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,16 +1,12 @@
 import { _getInitialData } from "../utils/_DataAPI";
 import { getUsers } from "../actions/users";
 import { getQuestions } from "../actions/questions";
-import { setLoggedUser } from "../actions/auth";
-
-const AUTHED_ID = "tylermcginnis";
 
 export function handleInitialData() {
   return (dispatch) => {
     return _getInitialData().then(({ usersData, quetionsData }) => {
       dispatch(getUsers(usersData));
       dispatch(getQuestions(quetionsData));
-      dispatch(setLoggedUser(AUTHED_ID));
     });
   };
 }
