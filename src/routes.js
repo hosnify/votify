6d30d3acf2ececbb580fc35bb3177c4154835f2b
hosnify <hosnify@gmail.com>
2021-06-20 21:24:26 +0200

    add new routes and navigation

--- src/routes.js
+++ src/routes.js
@@ -1,23 +1,36 @@
 import { Navigate } from "react-router-dom";
-
+import AddQuestionPage from "./components/AddQuestionPage";
 import Dashboard from "./components/Dashboard";
 import Leaderboard from "./components/Leaderboard";
 import Login from "./components/Login";
+import QuestionDetails from "./components/QuestionDetails";
 
 const routes = (user) => {
   console.log(user);
   return [
     {
       path: "/",
-      element: user ? <Dashboard /> : <Navigate to="/login" />,
+      element: user ? <Dashboard /> : <Login />,
     },
     {
       path: "/leaderboard",
-      element: user ? <Leaderboard /> : <Navigate to="/login" />,
+      element: user ? <Leaderboard /> : <Login />,
+    },
+    {
+      path: "/add",
+      element: user ? <AddQuestionPage /> : <Login />,
     },
     {
       path: "/login",
       element: <Login />,
+    },
+    {
+      path: "/questions/:question_id",
+      element: user ? <QuestionDetails /> : <Login />,
+    },
+    {
+      path: "*",
+      element: user ? <Navigate to="/"></Navigate> : <Login />,
     },
   ];
 };
