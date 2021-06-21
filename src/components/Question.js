bf7c70e7dfc153de39d7e71ee5a796b559821281
hosnify <hosnify@gmail.com>
2021-06-21 06:17:50 +0200

    handle case of undefined question

--- src/components/Question.js
+++ src/components/Question.js
@@ -8,7 +8,7 @@ import {
   CardContent,
   Typography,
 } from "@material-ui/core";
-import { Button, Icon, Progress, Transition, Modal } from "semantic-ui-react";
+import { Button, Icon, Progress, Transition } from "semantic-ui-react";
 
 import { useTheme } from "@material-ui/core/styles";
 import { handleAddAnswer } from "../actions/answer";
@@ -16,13 +16,15 @@ import { useState, useEffect, useRef } from "react";
 
 function Question({ loggedUser, users, question, dispatch, ...rest }) {
   const theme = useTheme();
+  const { question_id } = useParams();
   const [loading, setLoading] = useState(false);
   const [success, setSuccess] = useState(false);
-  const [answer, setAnswer] = useState(users[loggedUser].answers[question.id]);
+  const [answer, setAnswer] = useState(
+    !!question ? users[loggedUser].answers[question.id] : ""
+  );
 
   const timer = useRef();
   const navigate = useNavigate();
-  const { question_id } = useParams();
   useEffect(() => {
     return () => {
       clearTimeout(timer.current);
@@ -50,130 +52,139 @@ function Question({ loggedUser, users, question, dispatch, ...rest }) {
   const getVotes = (answer) => {
     return question[answer].votes.length;
   };
-  return (
-    <Grid container direction="row">
-      <Grid item>
-        <Box maxWidth={theme.spacing(30)}>
-          <Avatar
-            alt="avatar"
-            src={users[question.author].avatarURL}
-            style={{
-              width: theme.spacing(20),
-              height: theme.spacing(20),
-              margin: theme.spacing(5),
-            }}
-          ></Avatar>
-        </Box>
-      </Grid>
-      <Grid item lg>
-        <Card style={{ minWidth: theme.spacing(150), maxWidth: "fit-content" }}>
-          <CardContent>
-            <Typography color="primary" gutterBottom>
-              {users[question.author].name} asked :
-            </Typography>
-            <Typography color="primary" gutterBottom>
-              Would you rather ?
-            </Typography>
-            <Grid container direction="column" justify="center" spacing={5}>
-              <Grid item lg={6}>
-                <Button.Group>
-                  <Button
-                    loading={loading}
-                    disabled={!!users[loggedUser].answers[question.id]}
-                    positive={answer === "optionOne"}
-                    negative={answer === "optionTwo"}
-                    icon
-                    fluid
-                    onClick={() => handleAnswer(question.id, "optionOne")}
-                  >
-                    <Icon
-                      name={
-                        (answer === "optionOne" && "check") ||
-                        (answer === "optionTwo" && "close")
-                      }
-                    />
-                    {question.optionOne.text}
-                  </Button>
-                  <Button.Or />
-                  <Button
-                    disabled={!!users[loggedUser].answers[question.id]}
-                    loading={loading}
-                    positive={answer === "optionTwo"}
-                    negative={answer === "optionOne"}
-                    fluid
-                    onClick={() => handleAnswer(question.id, "optionTwo")}
-                  >
-                    <Icon
-                      name={
-                        (answer === "optionOne" && "close") ||
-                        (answer === "optionTwo" && "check")
-                      }
-                    />
-                    {question.optionTwo.text}
-                  </Button>
-                </Button.Group>
-              </Grid>
-              <Grid container direction="row" spacing={5} item lg={12}>
+  if (question) {
+    return (
+      <Grid container direction="row">
+        <Grid item>
+          <Box maxWidth={theme.spacing(30)}>
+            <Avatar
+              alt="avatar"
+              src={users[question.author].avatarURL}
+              style={{
+                width: theme.spacing(20),
+                height: theme.spacing(20),
+                margin: theme.spacing(5),
+              }}
+            ></Avatar>
+          </Box>
+        </Grid>
+        <Grid item lg>
+          <Card
+            style={{ minWidth: theme.spacing(150), maxWidth: "fit-content" }}
+          >
+            <CardContent>
+              <Typography component="div" color="primary" gutterBottom>
+                {users[question.author].name} asked :
+              </Typography>
+              <Typography component="div" color="primary" gutterBottom>
+                Would you rather ?
+              </Typography>
+              <Grid container direction="column" justify="center" spacing={5}>
                 <Grid item lg={6}>
-                  <Transition
-                    visible={!!users[loggedUser].answers[question.id]}
-                    animation="scale"
-                    duration={500}
-                  >
-                    <Progress
-                      value={getVotes("optionOne")}
-                      total={getVotes("optionOne") + getVotes("optionTwo")}
-                      progress="percent"
-                      precision={2}
-                      indicating
-                      label={` ${getVotes("optionOne")} of ${
-                        getVotes("optionOne") + getVotes("optionTwo")
-                      } votes `}
-                    />
-                  </Transition>
-                </Grid>
-                <Grid item lg={6}>
-                  <Transition
-                    visible={!!users[loggedUser].answers[question.id]}
-                    animation="scale"
-                    duration={500}
-                  >
-                    <Progress
-                      value={getVotes("optionTwo")}
-                      total={getVotes("optionOne") + getVotes("optionTwo")}
-                      progress="percent"
-                      precision={2}
-                      indicating
-                      label={` ${getVotes("optionTwo")} of ${
-                        getVotes("optionOne") + getVotes("optionTwo")
-                      } votes `}
-                    />
-                  </Transition>
-                </Grid>
-              </Grid>
-              {!users[loggedUser].answers[question.id] && (
-                <Grid item lg={12} container justify="flex-end">
-                  {question_id ? (
-                    <Button basic color="green" on onClick={confirmAnswer}>
-                      Confirm Vote
-                    </Button>
-                  ) : (
+                  <Button.Group>
                     <Button
-                      basic
-                      color="green"
-                      on
-                      onClick={() => navigate(`/questions/${question.id}`)}
+                      loading={loading}
+                      disabled={!!users[loggedUser].answers[question.id]}
+                      positive={answer === "optionOne"}
+                      negative={answer === "optionTwo"}
+                      icon
+                      fluid
+                      onClick={() => handleAnswer(question.id, "optionOne")}
                     >
-                      Show Question
+                      <Icon
+                        name={
+                          (answer === "optionOne" && "check") ||
+                          (answer === "optionTwo" && "close")
+                        }
+                      />
+                      {question.optionOne.text}
                     </Button>
-                  )}
+                    <Button.Or />
+                    <Button
+                      disabled={!!users[loggedUser].answers[question.id]}
+                      loading={loading}
+                      positive={answer === "optionTwo"}
+                      negative={answer === "optionOne"}
+                      fluid
+                      onClick={() => handleAnswer(question.id, "optionTwo")}
+                    >
+                      <Icon
+                        name={
+                          (answer === "optionOne" && "close") ||
+                          (answer === "optionTwo" && "check")
+                        }
+                      />
+                      {question.optionTwo.text}
+                    </Button>
+                  </Button.Group>
                 </Grid>
-              )}
-            </Grid>
-          </CardContent>
-        </Card>
+                <Grid container direction="row" spacing={5} item lg={12}>
+                  <Grid item lg={6}>
+                    <Transition
+                      visible={!!users[loggedUser].answers[question.id]}
+                      animation="scale"
+                      duration={500}
+                    >
+                      <Progress
+                        value={getVotes("optionOne")}
+                        total={getVotes("optionOne") + getVotes("optionTwo")}
+                        progress="percent"
+                        precision={2}
+                        indicating
+                        label={` ${getVotes("optionOne")} of ${
+                          getVotes("optionOne") + getVotes("optionTwo")
+                        } votes `}
+                      />
+                    </Transition>
+                  </Grid>
+                  <Grid item lg={6}>
+                    <Transition
+                      visible={!!users[loggedUser].answers[question.id]}
+                      animation="scale"
+                      duration={500}
+                    >
+                      <Progress
+                        value={getVotes("optionTwo")}
+                        total={getVotes("optionOne") + getVotes("optionTwo")}
+                        progress="percent"
+                        precision={2}
+                        indicating
+                        label={` ${getVotes("optionTwo")} of ${
+                          getVotes("optionOne") + getVotes("optionTwo")
+                        } votes `}
+                      />
+                    </Transition>
+                  </Grid>
+                </Grid>
+                {!users[loggedUser].answers[question.id] && (
+                  <Grid item lg={12} container justify="flex-end">
+                    {question_id ? (
+                      <Button basic color="green" onClick={confirmAnswer}>
+                        Confirm Vote
+                      </Button>
+                    ) : (
+                      <Button
+                        basic
+                        color="green"
+                        onClick={() => navigate(`/questions/${question.id}`)}
+                      >
+                        Show Question
+                      </Button>
+                    )}
+                  </Grid>
+                )}
+              </Grid>
+            </CardContent>
+          </Card>
+        </Grid>
       </Grid>
-    </Grid>
+    );
+  }
+  return (
+    <h2>
+      404 ... There was an error with this listing.please go back to the home
+      page .
+    </h2>
   );
 }
 
